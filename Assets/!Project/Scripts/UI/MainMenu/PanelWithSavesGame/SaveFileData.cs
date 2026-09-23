93df45e71c497c4a4378d086df0c94aa2f8ee60d
Dmitriy Zhuravlev <tankerlord@mail.ru>
2026-09-23 15:34:33 +0300

    SKI-130 delete save files

--- Assets/!Project/Scripts/UI/MainMenu/PanelWithSavesGame/SaveFileData.cs
+++ Assets/!Project/Scripts/UI/MainMenu/PanelWithSavesGame/SaveFileData.cs
@@ -1,22 +1,34 @@
 using TMPro;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 
 public class SaveFileData : MonoBehaviour
 {
     private int SaveFileIndex;
     [SerializeField] private TMP_Text fileName;
-    [SerializeField] private TMP_Text buttonName;
+    [SerializeField] private TMP_Text buttonLoadName;
+    [SerializeField] private GameObject buttonDelete;
+
+    private string filejsonUrl;
     public void LoadSystem(int index)
     {
         SaveFileIndex = index;
-        string filejsonUrl = $"{Application.persistentDataPath}/FilesSettings/Market_settings_{index}.json";
+        filejsonUrl = $"{Application.persistentDataPath}/FilesSettings/Market_settings_{index}.json";
         fileName.text = $"Сохранение {index}";
-        buttonName.text = File.Exists(filejsonUrl) ? "Загрузить" : "Новая игра";
+        bool hasSaveFile = File.Exists(filejsonUrl);
+        buttonLoadName.text = hasSaveFile ? "Загрузить" : "Новая игра";
+        if (!hasSaveFile) { buttonDelete.SetActive(false); }
     }
 
     public void LoadFile()
     {
         LoadGameData.Instance.LoadSettings(SaveFileIndex);
+    }
+
+    public void DeleteFile()
+    {
+        File.Delete(filejsonUrl);
+        LoadSystem(SaveFileIndex);
     }
 }
